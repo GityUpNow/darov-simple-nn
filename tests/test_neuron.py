ebd5c0893245c392618247507ce5d3f680781479
GityUpNow <kim-cheng@gmx.de>
2022-03-26 03:24:09 +0100

    feat: wrote neuron test

---
+++ tests/test_neuron.py
@@ -0,0 +1,21 @@
+import unittest
+
+from darov.components.neuron import Neuron
+import darov.components.activations as activations
+
+
+class TestNeuron(unittest.TestCase):
+
+    def test_linear(self):
+        neuron = Neuron(
+            [1.0, -0.5, 0.7],
+            0.2,
+            activations.linear,
+        )
+        input_values = [10.0, 1.0, -3.0]
+
+        self.assertEqual(neuron.calculate(input_values), 7.4)
+
+
+if __name__ == '__main__':
+    unittest.main()
